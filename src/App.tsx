6a9aeace5a3aceda75c48169e1527b30a896a231
Ricky-Android <ricky@levvels.io>
2026-08-10 14:00:11 +0900

    히어로 아래 저장 아이콘 구분선 제거

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { HowIWork } from './components/HowIWork'
 import { MoreWork } from './components/MoreWork'
-import { PixelDivider } from './components/PixelDivider'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
 import { TastePanel } from './components/TastePanel'
@@ -20,7 +19,6 @@ export default function App() {
       <Header />
       <main className={styles.shell} id="main-content">
         <Hero />
-        <PixelDivider />
         <Projects />
         <div className={styles.aboutSkills}>
           <About />
